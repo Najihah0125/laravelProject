bb1a60109e7ee3740b0d8257b5d46fc907212abb
Nur Najihah <nurnajihah.mrizal@petronas.com>
2022-12-14 21:55:46 +0800

    Install jetstream

--- finalProject/routes/web.php
+++ finalProject/routes/web.php
@@ -15,3 +15,13 @@ use App\Http\Controllers\homeControl;
 */
 
 Route::get('/home', [homeControl::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
