367911b43beab4a22998b67887b713b9e3dd925c
Nur Najihah <nurnajihah.mrizal@petronas.com>
2022-12-19 11:13:46 +0800

    Add "lect_id" column

--- finalProject/database/migrations/2014_10_12_000000_create_users_table.php
+++ finalProject/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lect_id');
             $table->string('email')->unique();
             $table->string('usertype')->default(0);
             $table->timestamp('email_verified_at')->nullable();
