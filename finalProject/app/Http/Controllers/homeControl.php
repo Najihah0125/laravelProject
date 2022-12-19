c316b760da2932907f1ca1ccc3d97ef055fe8b7d
Nur Najihah <nurnajihah.mrizal@petronas.com>
2022-12-19 09:25:06 +0800

    Delete unwanted codes

--- finalProject/app/Http/Controllers/homeControl.php
+++ finalProject/app/Http/Controllers/homeControl.php
@@ -58,31 +58,40 @@ class homeControl extends Controller
     }
 
     function dispListUpd(){
-        // $proj = Project::paginate(5);
-        // return view('supervisor.updateproject', ['list'=>$proj]);
-        
-        // $lect = Auth::user()->lect_id;
-
-        // if($lect == 'L001'){
-        //     $proj = DB::table('projects')->where('supervisor_id', 'L001');
-        //     return view('supervisor.updateproject', ['list'=>$proj]);
-        // }
-        // else if ($lect == '2'){
-        //     return view('users.supervisorpage');
-        // }
-        // else{
-        //     return view('home');
-        // }
-
         $lect = Auth::user()->lect_id;
 
         if($lect == 'L001'){
             $proj = DB::select('select * from projects where supervisor_id = ?', ['L001']);
             return view('supervisor.updateproject', ['list'=>$proj]);
         }
-       
         else{
             return view('home');
         }
+    }
+
+    function editProj($id){
+        $proj = Project::find($id);
+        return view('supervisor.editprojectpage', ['x'=>$proj]);
+    }
+
+    function editedProj(Request $req){
+        $proj = Project::find($req->project_id);
+        
+        $proj->project_id = $req->project_id;
+        $proj->title = $req->title;
+        $proj->category = $req->category;
+        $proj->student_id = $req->student_id;
+        $proj->supervisor_id = $req->supervisor_id;
+        $proj->examineer1_id = $req->examineer1_id;
+        $proj->examineer2_id = $req->examineer2_id;
+        $proj->start_date = $req->start_date;
+        $proj->end_date = $req->end_date;
+        $proj->duration = $req->duration;
+        $proj->progress = $req->progress;
+        $proj->status = $req->status;
+
+        $proj->save();
+
+        return redirect('updatelistpage'); 
     }
 }
